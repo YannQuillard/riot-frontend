16e610b99fb6249c723c6e4e8099621704c2fab5
GitZiarys <jonathan.bories-ronziere@hetic.net>
2021-11-30 14:55:02 +0100

    Adding Header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+
+function App() {
+  return (
+    <body>
+      <div className="App">
+        <Header />
+      </div>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+// src/Title.js
+
+import React from 'react'
+import './header.css';
+
+function Header() {
+  return (
+    <div className="container">
+        <div className="header-img"></div>
+    </div>
+  )
+}
+
+export default Header
