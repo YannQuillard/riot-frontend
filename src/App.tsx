63a8db81abee3398e1624acf1db9832f158b8827
GitZiarys <jonathan.bories-ronziere@hetic.net>
2021-12-01 15:39:15 +0100

    Add From (design only) + create global css properties

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/header';
 import Nav from './components/navigation+toogle/Nav';
 import TON from './components/navigation+toogle/toogle_off_navbar'
+import Inscription from './components/Home-inscription/home-inscription';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <TON />
         <Nav />
         <Header />
+        <Inscription />
       </div>
     </body>
   );
