1ef0d1cf57e4d850c21082775509436630021791
GitZiarys <jonathan.bories-ronziere@hetic.net>
2021-12-02 18:28:27 +0100

    Add Research Champion section (design only + bug fixes

---
+++ src/components/Favorites - Fav/Favorites - NavBar/favorites-buttons.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function NavButtons() {
+  const [toggleState, setToggleState] = useState(6);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+        <div>
+            <button className={toggleState === 1 ? "FavNav_Icon FavNav_Icon_Active" : "FavNav_Icon"} id="FavNav_toplane" onClick={() => toggleTab(1)}></button>
+            <button className={toggleState === 2 ? "FavNav_Icon FavNav_Icon_Active" : "FavNav_Icon"} id="FavNav_jungle" onClick={() => toggleTab(2)}></button>
+            <button className={toggleState === 3 ? "FavNav_Icon FavNav_Icon_Active" : "FavNav_Icon"} id="FavNav_middlane" onClick={() => toggleTab(3)}></button>
+            <button className={toggleState === 4 ? "FavNav_Icon FavNav_Icon_Active" : "FavNav_Icon"} id="FavNav_botlane" onClick={() => toggleTab(4)}></button>
+            <button className={toggleState === 5 ? "FavNav_Icon FavNav_Icon_Active" : "FavNav_Icon"} id="FavNav_support" onClick={() => toggleTab(5)}></button>
+            <button className={toggleState === 6 ? "FavNav_Icon FavNav_Icon_Active" : "FavNav_Icon"} id="FavNav_all" onClick={() => toggleTab(6)}></button>
+        </div>
+  );
+}
+
+export default NavButtons
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../App.css';
+import '../components/Favorites - Fav/FavGlobal.css';
 import FavHeader from '../components/Favorites - Header/favorites-header';
-import PlayerFav from '../components/Favorites - Fav/favorites-playerFav';
+import PlayerFav from '../components/Favorites - Fav/Favorites-PlayerFav/favorites-playerFav';
+import FavNav from '../components/Favorites - Fav/Favorites - NavBar/favorites-navbar';
 
 function Favorites() {
   return (
     <body>
       <div className="App">
         <FavHeader />
-        <PlayerFav />
+        <div className="FavGlobal"> 
+          <PlayerFav />
+          <FavNav />
+        </div>
       </div>
     </body>
   );
