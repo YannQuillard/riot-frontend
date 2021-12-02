69fb683857f31ffc557d9330010a69779ae44bb0
GitZiarys <jonathan.bories-ronziere@hetic.net>
2021-12-02 14:42:09 +0100

    Add Favorite Player's champion (design only)

---
+++ src/components/Favorites - Header/favorites-header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./favorites-header.css";
+
+
+function FavHeader(){
+    return(
+        <div className="container_Fav_Header">
+            <p className="background_Fav_Header_Text"> Gérez vos favoris </p>
+            <div className="background_Fav_Header"></div>
+        </div>
+    );
+}
+
+export default FavHeader
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../App.css';
+import FavHeader from '../components/Favorites - Header/favorites-header';
+import PlayerFav from '../components/Favorites - Fav/favorites-playerFav';
 
 function Favorites() {
   return (
     <body>
       <div className="App">
+        <FavHeader />
+        <PlayerFav />
       </div>
     </body>
   );
